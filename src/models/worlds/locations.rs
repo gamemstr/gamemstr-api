00593d680c70dead6b8af0bca5fadd7e9f908995
James Wolff <james.wolff0@gmail.com>
2023-04-01 18:37:48 -0500

    impl Model for Location

--- src/models/worlds/locations.rs
+++ src/models/worlds/locations.rs
@@ -13,3 +13,29 @@ pub struct Location {
     pub map_coordinates: serde_json::Value,
     pub npcs: serde_json::Value,
 }
+
+impl super::super::Model for Location {
+    type Entity = gamemstr_common::world::location::Location;
+
+    fn new(entity: Self::Entity) -> Self {
+        Self {
+            id: entity.id,
+            name: entity.name,
+            description: entity.description,
+            world_id: entity.world_id,
+            map_coordinates: serde_json::to_value(entity.map_coordinates).unwrap(),
+            npcs: serde_json::to_value(entity.npcs).unwrap(),
+        }
+    }
+
+    fn to_entity(&self) -> Self::Entity {
+        Self::Entity {
+            id: self.id.clone(),
+            name: self.name.clone(),
+            description: self.description.clone(),
+            world_id: self.world_id.clone(),
+            map_coordinates: serde_json::from_value(self.map_coordinates.clone()).unwrap(),
+            npcs: serde_json::from_value(self.npcs.clone()).unwrap(),
+        }
+    }
+}
