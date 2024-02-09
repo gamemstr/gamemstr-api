80179b6e0867d457c2aaa8025be3205138804a02
James Wolff <james.wolff0@gmail.com>
2024-02-09 14:26:46 +0000

    Refactor campaigns table schema

--- migrations/2024-02-08-220006_campaigns/up.sql
+++ migrations/2024-02-08-220006_campaigns/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE campaigns (
-  id VARCHAR PRIMARY KEY,
-  name VARCHAR NOT NULL,
+  id          VARCHAR PRIMARY KEY,
+  name        VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
-  world_id VARCHAR NOT NULL,
-  players JSONB NOT NULL
+  world_id    VARCHAR NOT NULL,
+  players     JSONB NOT NULL
 );
